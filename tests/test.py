ce2ae1f44e1e4fbb3ce09475d8ddfd85873e0b1f
EXP <289065406@qq.com>
2021-12-29 01:27:44 +0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 # ----------------------------------------------------------------------
 
 import unittest
-from src._crypt import *
+from src.crypt import *
 from src.admin import *
 from src.user import *
 
